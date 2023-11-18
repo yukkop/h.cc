eec20f3d9b1ebdb01182266329f39a1f9d48f672
yukkop <hectic.yukkop.it@gmail.com>
2023-11-18 15:30:01 +0000

    fix: move_to.lua

--- startup/turtle/move_to.lua
+++ startup/turtle/move_to.lua
@@ -1,25 +1,47 @@
 -- Turtle script to build a sphere
 
 -- Directions
-local Directions = {
+local Direction = {
     NORTH = 1, -- -y
-    WEST = 2 -- -x
+    WEST = 2, -- -x
     SOUTH = 3, -- +y
-    EAST = 4, -- +x
+    EAST = 4 -- +x
 }
 
 local args = {...}
 
 -- TODO inline arg
-local direction = Directions.NORTH
+local direction = Direction.NORTH
 
 local pos = {x = 0, y = 0}
 
--- Function to calculate the points of a circle for a given radius
-local function calculateCirclePoints(radius)
-    -- Implement the midpoint circle algorithm or a similar algorithm
-    -- to return a list of points (x, y) for the circle
-    {0, 0}
+local function turnLeft(n) 
+    for i = 1, n do
+        turtle.turnLeft()
+    end
+end
+
+local function turnRight(n) 
+    for i = 1, n do
+        turtle.turnRight()
+    end
+end
+
+local function rotate(from, to)
+    if from > to then
+	local dif = from - to 
+        if dif == 3 then
+	    turtle.turnLeft()
+        end
+	turnRight(dif)
+    end
+    if from > to then
+	local dif = to - from 
+        if dif == 3 then
+	    turtle.turnRight()
+        end
+	turnLeft(dif)
+    end
 end
 
 -- Function to move the Turtle to a specific point
@@ -46,35 +68,6 @@ local function moveTo(x, y)
     end
     turtle.forward(yDist)
 
-end
-
-local function rotate(from, to)
-    if from > to then
-	local dif = from - to 
-        if dif == 3 then
-	    turtle.turnLeft()
-        end
-	turnRight(dif)
-    end
-    if from > to then
-	local dif = to - from 
-        if dif == 3 then
-	    turtle.turnRight()
-        end
-	turnLeft(dif)
-    end
-end
-
-local function turnLeft(n) 
-    for i = 1, n do
-        turtle.turnLeft()
-    end
-end
-
-local function turnRight(n) 
-    for i = 1, n do
-        turtle.turnRight()
-    end
 end
 
 local x = tonumber(args[1])
