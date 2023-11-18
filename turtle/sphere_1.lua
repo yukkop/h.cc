2ba333774b29ea104752282f4bffdffc8f5e2e26
yukkop <hectic.yukkop.it@gmail.com>
2023-11-18 17:17:07 +0000

    feat: print sphere for sphere_1.lua

--- turtle/sphere_1.lua
+++ turtle/sphere_1.lua
@@ -14,6 +14,12 @@ function createVoxelSphere(radius)
     return sphere
 end
 
+function printVoxelSphere(sphere)
+    for _, voxel in ipairs(sphere) do
+        print("Voxel: x=" .. voxel[1] .. ", y=" .. voxel[2] .. ", z=" .. voxel[3])
+    end
+end
+
 local args = {...}
 
-print(createVoxelSphere(tonumber(args[1]))
+printVoxelSphere(createVoxelSphere(tonumber(args[1])))
